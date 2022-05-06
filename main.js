600237c1d65fb1f6055748eccf105f85824b499a
DmitroAleksandrovich <104141322+DmitroAleksandrovich@users.noreply.github.com>
2022-05-06 15:02:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const dom = document.querySelector('span');
+dom.onclick = (evt) => {
+    evt.target.style.color = 'white'
+}
+const string = 'dfscgbdsf';
+const number = 5;
+const boolean = true;
+
+const array = [1, 2, "ljlij"];
+const object = {
+    field: 'sdfsdfs',
+    field2: 123,
+}
